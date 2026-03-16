cab153d0971f1f6653dd47e5ee374975d3986f92
Dan <qwazar2000@gmail.com>
2026-03-16 14:11:48 +0100

    Enhance layout and styling: Added light pink color variable to globals.css, applied Poppins font to body in layout.tsx, and updated Home component with padding and styling.

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Home = () =>
 {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="p-10" bg-shop-light-pink>
+      <h2 className="text-xl font-semibold">Home</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
         ratione accusantium. Consequuntur fugiat sed culpa quis quidem quod
