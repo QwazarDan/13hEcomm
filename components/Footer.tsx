da3038a11537961eb46c3c4357bd9464de458534
Dan <qwazar2000@gmail.com>
2026-06-05 18:08:15 +0200

    Enhance Footer component with new structure and content: Integrated FooterTop, Logo, and SocialMedia components for improved layout and user engagement, while updating the footer's styling for a cohesive design.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,31 @@
-import React from "react";
 import Container from "./Container";
+import FooterTop from "./FooterTop";
+import Logo from "./Logo";
+import SocialMedia from "./SocialMedia";
 
 const Footer = () => {
   return (
-    <footer>
-      <Container>Footer</Container>
+    <footer className="bg-white border-t">
+      <Container>
+        <FooterTop />
+        <div>
+          <div>
+            <Logo />
+            <p>
+              Discover curated furniture collections at Shopcaryt, bleending
+              style and comfort to elevate your living spaces.
+            </p>
+            <SocialMedia
+              className="text-darkColor/60"
+              iconClassName="border-darkColor/60 hover:border-shop_light_green hover:text-shop_light_green"
+              tooltipClassName="bg-darkColor text-white"
+            />
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>j
+      </Container>
     </footer>
   );
 };
