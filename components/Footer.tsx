bc268e3955f6f172a7181fdb7e6c8aad997d12af
Dan <qwazar2000@gmail.com>
2026-06-07 23:22:37 +0200

    Enhance Footer component with quick links and new text elements: Updated layout to include SubText and SubTitle for improved content presentation, and integrated quick links for better navigation. Added new text components for consistent styling across the application.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,29 +2,41 @@ import Container from "./Container";
 import FooterTop from "./FooterTop";
 import Logo from "./Logo";
 import SocialMedia from "./SocialMedia";
+import { SubText, SubTitle } from "./ui/text";
+import Link from "next/link";
+import { quickLinksData } from "@/constants/data";
 
 const Footer = () => {
   return (
     <footer className="bg-white border-t">
       <Container>
         <FooterTop />
-        <div>
-          <div>
+        <div className="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="space-y-4">
             <Logo />
-            <p>
+            <SubText>
               Discover curated furniture collections at Shopcaryt, bleending
               style and comfort to elevate your living spaces.
-            </p>
+            </SubText>
             <SocialMedia
               className="text-darkColor/60"
               iconClassName="border-darkColor/60 hover:border-shop_light_green hover:text-shop_light_green"
               tooltipClassName="bg-darkColor text-white"
             />
           </div>
+          <div>
+            <SubTitle>Quick Links</SubTitle>
+            <ul className = "space-y-3 mt-4">
+              {quickLinksData?.map((item) => (
+                <li key={item?.title}>
+                  <Link href={item?.href}>{item?.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
           <div></div>
           <div></div>
-          <div></div>
-        </div>j
+        </div>
       </Container>
     </footer>
   );
