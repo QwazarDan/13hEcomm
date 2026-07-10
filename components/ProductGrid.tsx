a31610e27c95cdfc405bf930db4e832f16a74515
Dan <qwazar2000@gmail.com>
2026-07-10 14:57:57 +0200

    Refactor ProductGrid component: Simplified product state initialization and updated query structure for improved readability and maintainability.

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -4,12 +4,15 @@ import { useState } from "react";
 import HomeTabBar from "./HomeTabBar";
 
 const ProductGrid = () => {
-  const [products, setProducts] = useState<
-    { _id: string; name: string; price: number }[]
-  >([]);
+  const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
 
+  const query = `*[_type == "product" && variant == $variant ] | order(name desc){
+  name, ..., "categories":categories[]->title
+}`;
+
+  const params = { variant: selectedTab.toLowerCase() };
   return (
     <div>
       <HomeTabBar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
